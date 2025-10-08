5ef9d0c9d19e26a75586ab0f92b0c51ad9282598
Zwk08 <zkessle1@terpmail.umd.edu>
2025-10-07 22:11:48 -0400

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -29,7 +29,7 @@ for p in pins:
 
 
 
-GPIO.add_event_detect(button_pin, GPIO.RISING,  callback=fn, bouncetime=200)
+GPIO.add_event_detect(button_pin, GPIO.RISING,  callback=button_pressed_callback, bouncetime=200)
 
 try:
     while True:
@@ -42,10 +42,5 @@ except KeyboardInterrupt:
     for l in led:
         l.stop()
     GPIO.cleanup()
-
-
-
-
-
 
 
