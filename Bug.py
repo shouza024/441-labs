90717a0be16ffe760108726986571bf547a3cb24
Zwk08 <zkessle1@terpmail.umd.edu>
2025-10-20 12:42:10 -0400

    Update Bug.py

--- Bug.py
+++ Bug.py
@@ -11,8 +11,7 @@ class Bug:
 		self.__shifter = Shifter(23,24,25)
 		self._running = False
 
-
-    def start(self):
+	def start(self):
         self._running = True 
         while self._running:
         	self.__shifter.shiftByte(1<<self.x)
