49108b3c71ba1015ea3ce7947cc9853fd60b2dd4
Zwk08 <zkessle1@terpmail.umd.edu>
2025-10-20 12:28:50 -0400

    Update shifter.py

--- shifter.py
+++ shifter.py
@@ -3,17 +3,13 @@ import time
 
 class Shifter:
     def __init__(self, serialPin,latchPin,clockPin):#for thread code conductor 
-    self.serialPin = serialPin
-    self.latchPin = latchPin #store the numbers of pins as attributes
-    self.clockPin = clockPin
-
-GPIO.setmode(GPIO.BCM)
-
-
-
-GPIO.setup(dataPin, GPIO.OUT)
-GPIO.setup(latchPin, GPIO.OUT, initial=0)  # start latch low
-GPIO.setup(clockPin, GPIO.OUT, initial=0)  # start clock low
+        self.serialPin = serialPin
+        self.latchPin = latchPin #store the numbers of pins as attributes
+        self.clockPin = clockPin
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(dataPin, GPIO.OUT)
+        GPIO.setup(latchPin, GPIO.OUT, initial=0)  # start latch low
+        GPIO.setup(clockPin, GPIO.OUT, initial=0)  # start clock low
 
 
 def __ping(self,pin):  # ping the clock or latch pin private the method
