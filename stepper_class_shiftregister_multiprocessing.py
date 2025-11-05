47acddc510103e7838bff96453c6c74158a4d42c
Zwk08 <zkessle1@terpmail.umd.edu>
2025-11-05 16:51:29 -0500

    Update stepper_class_shiftregister_multiprocessing.py

--- stepper_class_shiftregister_multiprocessing.py
+++ stepper_class_shiftregister_multiprocessing.py
@@ -65,13 +65,13 @@ class Stepper:
 
     # Move relative angle from current position:
     def __rotate(self, delta):
-        #self.lock.acquire()                 # wait until the lock is available
+        self.lock.acquire()                 # wait until the lock is available
         numSteps = int(Stepper.steps_per_degree * abs(delta))    # find the right # of steps
         dir = self.__sgn(delta)        # find the direction (+/-1)
         for s in range(numSteps):      # take the steps
             self.__step(dir)
             time.sleep(Stepper.delay/1e6)
-        #self.lock.release()
+        self.lock.release()
 
     # Move relative angle from current position:
     def rotate(self, delta):
